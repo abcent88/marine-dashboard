7741d7e4bc5b3f6ed899cc1b24d4a858ecbca5ba
Msugh Abraham <msughabraham@gmail.com>
2026-09-07 12:18:24 +0100

    Fix development session cookie security

--- node/server.js
+++ node/server.js
@@ -51,7 +51,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production" && process.env.APP_ENV !== "development",
     maxAge: 1000 * 60 * 60 * 8
   }
 }));
