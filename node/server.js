ee63ced191808b62a85106579fedbb2346afca6d
Msugh Abraham <msughabraham@gmail.com>
2026-09-08 22:02:43 +0100

    Protect metrics endpoint with authentication

--- node/server.js
+++ node/server.js
@@ -83,7 +83,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/ais", aisRoutes);
 
-app.use("/metrics", metricsRoutes);
+app.use("/metrics", requireAuth, metricsRoutes);
 
 app.get("/health", (req, res) => {
   res.json({
